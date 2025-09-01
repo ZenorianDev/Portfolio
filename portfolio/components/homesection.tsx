dd147b1386583ba7dc67a002b9661e61090a84c4
urhooman <urhooman18@gmail.com>
2025-09-01 17:09:00 +0800

    src, homesection

--- portfolio/components/homesection.tsx
+++ portfolio/components/homesection.tsx
@@ -1,7 +1,8 @@
 export default function HomeSection() {
   return (
     <section className="h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">I'm Robert Caro</h1>
+      <h1 className="text-5xl font-bold">Reanne Lorraine Rodriguez Martinez</h1>
+      <p className="text-xl mt-4">Aspiring Full Stack Developer and Game Developer</p>
     </section>                                
   );
 }
