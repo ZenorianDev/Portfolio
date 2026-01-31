842ab640688d413cf8bca68692df0f44f8a19a05
urhooman <urhooman18@gmail.com>
2026-01-31 11:16:01 +0800

    added new component and deleted code

--- portfolio/components/footer.tsx
+++
@@ -1,4 +0,0 @@
-// File: /components/footer.tsx
-export default function Footer() {
-return <footer className="py-16 text-center text-xs text-neutral-500">© {new Date().getFullYear()} Reanne Martinez</footer>;
-}
